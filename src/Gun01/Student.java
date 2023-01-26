496a81cf666dde38873909202d97e538bab8cf51
merve <mervegz263@gmail.com>
2023-01-26 21:36:27 +0300

    merve nin ilk task ı yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,6 +4,9 @@ public class Student {
     public static void main(String[] args) {
 
         System.out.println("Merhaba");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Merhaba");
+        }
 
     }
 }
